8468d83b972ab28c8a282bf12fc72b02b4f367ca
Bimsarani <bimbaalahakoon@gmail.com>
2025-03-22 16:49:46 +0530

    first commit

---
+++ server/festivo/src/test/java/com/example/festivo/FestivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.festivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
