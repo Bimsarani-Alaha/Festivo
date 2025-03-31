b4102acfe7d5bb873b77edf2f3bfd0a9ef7ab751
Praveen pramodh <IT22242372@MY.SLIIT.LK>
2025-03-31 21:51:25 +0530

    new

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import "./index.css";
-
-// User Components
-import SignUp from './Components/views/User/SignUp';
-import LoginForm from './Components/views/User/Login';
-import FeedbackPage from './Components/views/User/Feedback/Feedback';
-import UserManagementDashboard from './Components/views/Admin/UserManagement';
-
+import SignUp from '../src/Components/views/User/SignUp';
+import LoginForm from '../src/Components/views/User/Login';
+import "./index.css"
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/login" element={<LoginForm />} />
-        <Route path="/FeedbackPage" element={<FeedbackPage />} />
-        <Route path="/UserManagementDashboard" element={<UserManagementDashboard />} />
-       
       </Routes>
     </Router>
   );
